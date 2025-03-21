de3a08d9d7f7af38e884427a801a26f4570b4290
Marcus Vinicius Bertoncello <markaumvb@gmail.com>
2025-03-20 22:08:10 -0300

    ok

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -1,5 +1,16 @@
-// packages/database/index.ts
 import { PrismaClient } from "@prisma/client";
+import { Decimal as DecimalJS } from "@prisma/client/runtime/library";
 
+// Re-exportar todos os tipos do Prisma
 export * from "@prisma/client";
+
+// Exportar Decimal para compatibilidade
+export { DecimalJS as Decimal };
+
+// Adicionar o tipo Decimal ao namespace Prisma
+export namespace Prisma {
+  export type Decimal = DecimalJS;
+}
+
+// Exportar o cliente Prisma
 export const prisma = new PrismaClient();
