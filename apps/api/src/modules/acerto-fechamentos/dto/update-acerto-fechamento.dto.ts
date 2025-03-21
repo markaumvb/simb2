360606cde3dd3cfb9c3dc5aed4fbe96f9372135b
Marcus Vinicius Bertoncello <markaumvb@gmail.com>
2025-03-21 10:26:42 -0300

    ok

--- apps/api/src/modules/acerto-fechamentos/dto/update-acerto-fechamento.dto.ts
+++ apps/api/src/modules/acerto-fechamentos/dto/update-acerto-fechamento.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateAcertoFechamentoDto } from './create-acerto-fechamento.dto';
 
-export class UpdateAcertoFechamentoDto extends PartialType(CreateAcertoFechamentoDto) {}
+export class UpdateAcertoFechamentoDto extends PartialType(
+  CreateAcertoFechamentoDto,
+) {}
